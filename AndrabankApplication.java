77317a2cca776d3133851f9438b48e1ac7f54f26
kiranbujji <102509237+kiranbujji@users.noreply.github.com>
2022-04-01 13:30:16 +0530

    Add files via upload

---
+++ AndrabankApplication.java
@@ -0,0 +1,15 @@
+package com.crud.andrabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrabankApplication.class, args);
+	System.out.println("welcome to andhra bank  ->->->  ");
+	}
+
+}
+ 
